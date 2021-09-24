487ad4bb11c0b1982e4e221e4f5cf899fd7dabdd
Kin Chang <kinwo.chang@sjsu.edu>
2021-09-24 01:03:06 +0000

    Update makefile for Ubuntu, replace auto with int

--- hw2/countHits.cpp
+++ hw2/countHits.cpp
@@ -2,13 +2,13 @@
 
 int countHits(vector<Asteroid> a){
     vector<Asteroid> ast_hits;
-    for (auto i=0; i < a.size(); i++) {
+    for (int i=0; i < a.size(); i++) {
         Asteroid asti = a.at(i);
         if (asti.d == right) {
             ast_hits.push_back(asti);
         }
         if (asti.d == left) {
-            for (auto j=0; j < ast_hits.size(); j++) {
+            for (int j=0; j < ast_hits.size(); j++) {
                 if (asti.m >= ast_hits.at(j).m) {
                     ast_hits.erase(ast_hits.begin() + j);  // can erase (destroy) since we don't need it anymore 
                 }
