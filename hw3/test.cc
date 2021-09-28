ad43c44fb49979d47ad52ca82f3d0010b1b3e7f4
Kin Chang <kinwo.chang@sjsu.edu>
2021-09-28 19:56:11 +0000

    add test cases

--- hw3/test.cc
+++ hw3/test.cc
@@ -4,14 +4,30 @@
 using std::cout;
 using std::endl;
 
+
 int main() {
-  // string a = "13579";
-  // string b = "2468d";
   string a = "ace";
   string b = "bdf";
   string c = mergeStrings(a, b);
 
-  cout << "mergedString is: " << c;
+  cout << "a is: " << a << endl;
+  cout << "b is: " << b << endl;
+  cout << "mergedString is: " << c << endl << endl;
+
+  a = "13579";
+  b = "2468";
+  c = mergeStrings(a, b);
+  cout << "a is: " << a << endl;
+  cout << "b is: " << b << endl;
+  cout << "mergedString is: " << c << endl << endl;
+
+  a = "9753";
+  b = "86421";
+  c = mergeStrings(a, b);
+
+  cout << "a is: " << a << endl;
+  cout << "b is: " << b << endl;
+  cout << "mergedString is: " << c << endl << endl;
 
   return 0;
 }
