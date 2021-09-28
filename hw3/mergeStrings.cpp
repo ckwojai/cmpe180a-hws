ca8fe0868bc30a50977d3382501d0de21a52837f
Kin <kin@macbook-air.lan>
2021-09-28 12:50:24 -0700

    can't concatenate char using + operator

--- hw3/mergeStrings.cpp
+++ hw3/mergeStrings.cpp
@@ -10,16 +10,19 @@ string mergeStrings(const string &a, const string &b) {
   int len_b = b.length();
   if (len_a == len_b) {
     for (int i=0; i < len_a; i++) {
-      c += a[i] + b[i];
+      c += a[i];
+      c += b[i];
     }
   } else if (len_a > len_b) {
     for (int i=0; i < len_b; i++) {
-      c += a[i] + b[i];
+      c += a[i];
+      c += b[i];
     }
     c += a.substr(len_b, len_a-len_b);
   } else if (len_a < len_b) {
     for (int i=0; i < len_a; i++) {
-      c += a[i] + b[i];
+      c += a[i];
+      c += b[i];
     }
     c += b.substr(len_a, len_b-len_a);
   }
