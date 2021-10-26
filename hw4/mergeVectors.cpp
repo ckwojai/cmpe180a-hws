7cdc6a472cb928d163fdff360ae42451417ef9d0
Kin <kin@MacBook-Air.local>
2021-10-25 18:19:04 -0700

    hw4 first commit

---
+++ hw4/mergeVectors.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iterator>
+#include "mergeVectors.h"
+
+void mergeVectors(const vector<vector<int>>& input, vector<int>& result) {
+  int num_of_vectors = input.size();
+  vector<vector::const_iterator> vec_iters;
+  for (auto it=input.cbegin(); it!=input.cend(); it++) {
+    vec_iters.push_back((*it).cbegin())
+  }
+  bool done = false;
+  while (!done) {
+    vector<int> to_sort_ints;
+    int all_done = 0;
+    for(auto it=vec_iters.cbegin(); it!=it.cend(); it++) {
+      int index = std::distance(vec_iters.cbegin(), it);
+      if (*it != input.at(index).cend()) {
+        to_sort_ints.push_back(*(*it));
+      } else {
+        to_sort_ints.push_back(INT_MAX);
+        all_done++;
+      }
+    }
+    if (all_done == 4) { // When all vec_iters reaches the end
+      done = true;
+    } else {
+      auto it = std::min_element(to_sort_ints.cbegin(), to_sort_ints.cend());
+      int min_index = std::distance(to_sort_ints.cbegin(), it);
+      result.push_back(to_sort_ints.at(min_index));
+      vec_iters.at(min_index)++;
+    }
+  }
+}
---
+++ hw4/mergeVectors.h
@@ -0,0 +1,10 @@
+#ifndef __MERGEVECTORS_H_
+#define __MERGEVECTORS_H_
+
+#include <vector>
+using std::vector;
+
+
+void mergeStrings(const vector<vector<int>>& input, vector<int>& result);
+
+#endif // __MERGESTRINGS_H_
