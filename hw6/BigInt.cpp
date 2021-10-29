bfa4a7cd15c1b9f5bb7ca0d02c686d5f1f43da92
Kin Chang <kinwo.chang@sjsu.edu>
2021-10-29 03:01:30 +0000

    beautifully done

--- hw6/BigInt.cpp
+++ hw6/BigInt.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 #include "BigInt.h"
+
 
 void BigInt::init(const vector<int>& vec_int) {
   for(auto it=vec_int.cbegin(); it!=vec_int.cend(); it++) {
@@ -10,9 +12,9 @@ void BigInt::init(const vector<int>& vec_int) {
         exit(0);
       }
     if (it == vec_int.cbegin()) { // First element, okay to be negative
-      if (digit <= 0) {
+      if (digit < 0) {
         digits.push_back('-');
-        digits.push_back('0' + abs(digit));
+        digits.push_back('0' + std::abs(digit));
       } else {
         digits.push_back('+');
         digits.push_back('0' + digit);
@@ -20,7 +22,7 @@ void BigInt::init(const vector<int>& vec_int) {
     } else {
       if (digit < 0) {
         std::cerr<<"negative digits, using abs() to convert\n";
-        digits.push_back('0' + abs(digit));
+        digits.push_back('0' + std::abs(digit));
       } else {
         digits.push_back('0' + digit);
         }
@@ -30,16 +32,6 @@ void BigInt::init(const vector<int>& vec_int) {
 
 BigInt::BigInt(const vector<int>& vec_int) {
   init(vec_int);
-}
-ostream& operator<<(ostream& os, const BigInt& bi) {
-  for(auto it=bi.digits.cbegin(); it!=bi.digits.cend(); it++) {
-    os << *it;
-  }
-  os << std::endl;
-  return os;
-}
-istream& operator>>(istream& is, BigInt& dt) {
-  return is;
 }
 
 BigInt::BigInt(const vector<char>& vec_char) {
@@ -66,6 +58,16 @@ BigInt::BigInt(const vector<char>& vec_char) {
   }
   init(vec_int);
 }
+ostream& operator<<(ostream& os, const BigInt& bi) {
+  for(auto it=bi.digits.cbegin(); it!=bi.digits.cend(); it++) {
+    os << *it;
+  }
+  return os;
+}
+istream& operator>>(istream& is, BigInt& dt) {
+  return is;
+}
+
 BigInt::BigInt(int[], int arr_s) {
 }
 BigInt::BigInt(char[], int arr_s) {
@@ -118,7 +120,6 @@ BigInt BigInt::operator+(const BigInt& r) const {
         extra = 1;
         result.push_back((sd-10) + '0');
       } else {
-        std::cout << sd << std::endl;
         result.push_back(sd + '0');
         extra = 0;
       }
@@ -173,11 +174,11 @@ BigInt BigInt::operator-(const BigInt& r) const {
         int ld = *lit - '0';
         if (borrow == 1) {
           if (ld-borrow >= 0) {
-            result.push_back(ld-borrow);
+            result.push_back((ld-borrow) + '0');
             borrow = 0;
           } else {
             borrow = 1;
-            result.push_back('0');
+            result.push_back('9');
           }
         } else {
           result.push_back(ld + '0');
@@ -196,7 +197,7 @@ BigInt BigInt::operator-(const BigInt& r) const {
   } else if (ls=='+' && rs=='-') { // pos1 - neg2, which is the same as pos1 + abs(neg2)
     return *this + r.abs();
   } else if (ls=='-' && rs=='+') { // neg1 - pos2, which is the same as neg1 + neg(pos2)
-    return (*this - r.neg());
+    return (*this + r.neg());
   }
 }
 bool BigInt::operator==(const BigInt& r) const {
--- hw6/test.cc
+++ hw6/test.cc
@@ -1,13 +1,20 @@
 #include "BigInt.h"
+void test(const vector<char>lv, const vector<char>& rv) {
+  BigInt l(lv);
+  BigInt r(rv);
+  std::cout << "l: " << l << "; r: " << r << std::endl;
+  std::cout << "l + r = " << l + r << std::endl;
+  std::cout << "l - r = " << l - r << std::endl;
+  std::cout << "r - l = " << r - l << std::endl;
+}
 
 int main(){
-  vector<int> test = {1, 2, 3};
-  BigInt bi(test);
-  std::cout << bi;
-  vector<char> test2 = {'9'};
-  BigInt bi2(test2);
-  std::cout << bi2;
-  BigInt sum = bi + bi2;
-  std::cout << sum;
+  vector<char> lv = {'-', '1', '2', '3'};
+  vector<char> rv = {'-', '9'};
+  test(lv, rv);
+  lv = {'+', '1', '0', '0', '0', '0', '0', '0'};
+  rv = {'-', '9'};
+  test(lv, rv);
+  // std::cout << BigInt(lv) + BigInt(rv) << std::endl;
   return 0;
 }
