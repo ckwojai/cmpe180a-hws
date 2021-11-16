138bcde21d7edea60aef65e21d8f026a84c39c09
Kin Chang <kinchang@Kins-MacBook-Pro.local>
2021-11-15 18:26:54 -0800

    test file ... what a bad one

---
+++ hw6/realtest.cc
@@ -0,0 +1,39 @@
+#include "BigInt.h"
+void test(const vector<char>& lv, const vector<char>& rv) {
+  BigInt l(lv);
+  BigInt r(rv);
+  std::cout << "l: " << l << "; r: " << r << std::endl;
+  // std::cout << "++l = " << ++l << std::endl;
+  // std::cout << "--r = " << --r << std::endl;
+  std::cout << "l + r = " << l + r << std::endl;
+  std::cout << "l - r = " << l - r << std::endl;
+  std::cout << "r - l = " << r - l << std::endl;
+  std::cout << "l * r = " << l * r << std::endl;
+}
+
+int main(){
+  vector<char> lv = {'-', '1', '0'};
+  vector<char> rv = {'+', '2'};
+  test(lv, rv);
+  lv = {'+', '1', '0', '0', '0', '0', '0', '0'};
+  rv = {'-', '9'};
+  test(lv, rv);
+  lv = {'+', '0', '0', '0', '9', '9', '9', '9'};
+  rv = {'-', '9'};
+  test(lv, rv);
+  lv = {'+', '8'};
+  rv = {'+', '2'};
+  test(lv, rv);
+
+  char ca[4] = {'-', '1'};
+  int ia[3] = {1};
+  BigInt la(ca, 2);
+  BigInt ra(ia, 1);
+  std::cout << ++la << std::endl;
+  std::cout << --ra << std::endl;
+  // std::cout << BigInt(lv) + BigInt(rv) << std::endl;
+  BigInt bi;
+  std::cin >> bi;
+  std::cout << bi;
+  return 0;
+}
--- hw6/test.cc
+++ hw6/test.cc
@@ -1,39 +1,52 @@
-#include "BigInt.h"
-void test(const vector<char>& lv, const vector<char>& rv) {
-  BigInt l(lv);
-  BigInt r(rv);
-  std::cout << "l: " << l << "; r: " << r << std::endl;
-  // std::cout << "++l = " << ++l << std::endl;
-  // std::cout << "--r = " << --r << std::endl;
-  std::cout << "l + r = " << l + r << std::endl;
-  std::cout << "l - r = " << l - r << std::endl;
-  std::cout << "r - l = " << r - l << std::endl;
-  std::cout << "l * r = " << l * r << std::endl;
-}
+#include "BigInt.h" /* Your own header file */
+#include <iostream>
+#include <vector>
 
-int main(){
-  vector<char> lv = {'-', '1', '0'};
-  vector<char> rv = {'+', '2'};
-  test(lv, rv);
-  lv = {'+', '1', '0', '0', '0', '0', '0', '0'};
-  rv = {'-', '9'};
-  test(lv, rv);
-  lv = {'+', '0', '0', '0', '9', '9', '9', '9'};
-  rv = {'-', '9'};
-  test(lv, rv);
-  lv = {'+', '8'};
-  rv = {'+', '2'};
-  test(lv, rv);
+using namespace std;
 
-  char ca[4] = {'-', '1'};
-  int ia[3] = {1};
-  BigInt la(ca, 2);
-  BigInt ra(ia, 1);
-  std::cout << ++la << std::endl;
-  std::cout << --ra << std::endl;
-  // std::cout << BigInt(lv) + BigInt(rv) << std::endl;
-  BigInt bi;
-  std::cin >> bi;
-  std::cout << bi;
-  return 0;
+int main(int argc, const char *argv[])
+{
+    vector<int> integerVector;
+    integerVector.push_back(0);
+
+    vector<char> charVector;
+    charVector.push_back('5');
+
+    char charArray[] = {'5'};
+
+    BigInt A = BigInt(integerVector);
+     cout << "A = " << A << endl;
+
+     BigInt B = BigInt(charVector);
+     cout << "B = " << B << endl;
+
+    BigInt C = BigInt(charArray, 1);
+    cout << "C = " << C << endl;
+
+    BigInt D = B * C;
+
+    cout << "D = " << D << endl;
+    cout << "A+B = " << A + B << endl;
+    cout << "B-C = " << B - C << endl;
+
+    if (A == D)
+    {
+        cout << "A is equal to D " << endl;
+    }
+    else
+    {
+        cout << "A is not equal to D" << endl;
+    }
+
+    cout << "Is A>B? : " << (A > B) << endl;
+    cout << "Is B<C? : " << (B < C) << endl;
+    cout << "Is A>=D? : " << (A >= D) << endl;
+    cout << "Is C<=D? : " << (C <= D) << endl;
+    cout << "Output of Post Increment A: " << A++ << endl;
+    cout << "Output of Post Decrement A: " << A-- << endl;
+    cout << "Output of Pre Increment A: " << ++A << endl;
+    cout << "Output of Pre Decrement A: " << --A << endl;
+    cout << "What is ! of (A-D)? " << !(A - D) << endl;
+
+    return 0;
 }
