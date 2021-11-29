f38ec9c0d459b23e632e9f627d7813bd525b8fff
Kin Chang <kinchang@Kins-MacBook-Pro.local>
2021-11-29 09:04:02 -0800

    all shapes into shape.h and cpp

---
+++ hw8/Shape.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <math.h>
+
+#include "Shape.h"
+
+Circle::Circle(const XYpoint& icenter, const double& iradius) {
+  center = icenter;
+  radius = iradius;
+}
+
+double Circle::area() {
+  return radius * radius * M_PI;
+}
+
+Triangle::Triangle(const XYpoint& iv1, const XYpoint& iv2, const XYpoint& iv3) {
+  v1 = iv1;
+  v2 = iv2;
+  v3 = iv3;
+}
+
+double Triangle::area() {
+  return std::abs((v1.x*(v2.y-v3.y)+v2.x*(v3.y-v1.y)+v3.x*(v1.y-v2.y))/2);
+}
+
+Rectangle::Rectangle(const XYpoint& iv1, const XYpoint& iv2, const XYpoint& iv3, const XYpoint& iv4){
+  v1 = iv1;
+  v2 = iv2;
+  v3 = iv3;
+  v4 = iv4;
+}
+
+double Rectangle::area() {
+  // assuming rectangle points are given in a counter-clockwise manner
+  return (v1.x*v2.y + v2.x*v3.y + v3.x*v4.y + v4.x*v1.y - v1.x*v1.y - v3.x*v2.y - v4.x*v3.y - v1.x*v4.y) / 2;
+}
--- hw8/Shape.h
+++ hw8/Shape.h
@@ -1,9 +1,37 @@
 #ifndef SHAPE_H_
 #define SHAPE_H_
 
+#include "XYPoint.h"
+
+
 class Shape {
   public:
     virtual double area() = 0;
+};
+
+class Circle: public Shape {
+  public:
+    Circle(const XYpoint& icenter, const double& iradius);
+    double area();
+  private:
+    XYpoint center;
+    double radius;
+};
+
+class Triangle:public Shape {
+  public:
+    Triangle(const XYpoint& iv1, const XYpoint& iv2, const XYpoint& iv3);
+    double area();
+  private:
+    XYpoint v1, v2, v3;
+};
+
+class Rectangle: public Shape {
+  public:
+    Rectangle(const XYpoint& iv1, const XYpoint& iv2, const XYpoint& iv3, const XYpoint& iv4);
+    double area();
+  private:
+    XYpoint v1, v2, v3, v4;
 };
 
 #endif // SHAPE_H_
